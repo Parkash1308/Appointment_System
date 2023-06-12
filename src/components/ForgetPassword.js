06745f2c0bf055e44505e0c20091f1b1c169a13c
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-12 08:58:53 +0500

    Forget created

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -26,76 +26,53 @@ const ForgetPassword =()=>{
                                             <div className="form-outline mb-4">
 
                                                 <label className="form-label" htmlFor="form3Example1cg">
-                                                    Your Name
+                                                    CMS-ID
                                                 </label>
                                                 <input
                                                     type="text"
                                                     id="form3Example1cg"
-                                                    className="form-control form-control-lg"
+                                                    className="form-control "
+                                                    placeholder="Enter CMS-ID"
                                                 />
 
                                             </div>
+
                                             <div className="form-outline mb-4">
 
-                                                <label className="form-label" htmlFor="form3Example3cg">
-                                                    Your Email
-                                                </label>
-
-                                                <input
-                                                    type="email"
-                                                    id="form3Example3cg"
-                                                    className="form-control form-control-lg"
-                                                />
-
-                                            </div>
-                                            <div className="form-outline mb-4">
-                                                <input
-                                                    type="password"
-                                                    id="form3Example4cg"
-                                                    className="form-control form-control-lg"
-                                                />
                                                 <label className="form-label" htmlFor="form3Example4cg">
                                                     Password
                                                 </label>
-                                            </div>
-                                            <div className="form-outline mb-4">
+
                                                 <input
                                                     type="password"
-                                                    id="form3Example4cdg"
-                                                    className="form-control form-control-lg"
+                                                    id="form3Example4cg"
+                                                    className="form-control "
+                                                    placeholder="Enter Password"
                                                 />
+
+                                            </div>
+                                            <div className="form-outline mb-4">
+
                                                 <label className="form-label" htmlFor="form3Example4cdg">
                                                     Repeat your password
                                                 </label>
-                                            </div>
-                                            <div className="form-check d-flex justify-content-center mb-5">
                                                 <input
-                                                    className="form-check-input me-2"
-                                                    type="checkbox"
-                                                    defaultValue=""
-                                                    id="form2Example3cg"
+                                                    type="password"
+                                                    id="form3Example4cdg"
+                                                    className="form-control"
+                                                    placeholder="Enter Password"
                                                 />
-                                                <label className="form-check-label" htmlFor="form2Example3g">
-                                                    I agree all statements in{" "}
-                                                    <a href="#!" className="text-body">
-                                                        <u>Terms of service</u>
-                                                    </a>
-                                                </label>
+
                                             </div>
+
                                             <div className="d-flex justify-content-center">
                                                 <button
                                                     type="button"
-                                                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body"
+                                                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-white"
                                                 >
                                                     Register
                                                 </button>
                                             </div>
-                                            <p className="text-center text-muted mt-5 mb-0">
-                                                Have already an account?{" "}
-                                                <a href="#!" className="fw-bold text-body">
-                                                    <u>Login here</u>
-                                                </a>
-                                            </p>
                                         </form>
                                     </div>
                                 </div>
